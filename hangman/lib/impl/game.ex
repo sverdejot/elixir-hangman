85e1567e68b49e3ef5e633bb3ad2460ac9b00189
sverdejot <sverdejot@gmail.com>
2024-04-20 15:25:38 +0200

    feat(hangman): implemented whole hangman logic

--- hangman/lib/impl/game.ex
+++ hangman/lib/impl/game.ex
@@ -30,16 +30,60 @@ defmodule Hangman.Impl.Game do
   @spec make_guess(t, String.t) :: { t, Type.game_state }
   def make_guess(game = %{ game_state: state }, _guess) 
     when state in [:won, :lost] do
-    { game, game_state(game) }
+    game |> return_with_state
   end
+
+  def make_guess(game, guess) do
+    accept_guess(game, guess, MapSet.member?(game.used, guess)) 
+      |> return_with_state
+  end
+
+  defp accept_guess(game, _guess, _already_used = true) do
+    %{ game | game_state: :used }
+  end
+
+  defp accept_guess(game, guess, _already_used) do
+    %{ game | used: MapSet.put(game.used, guess) }
+      |> score_guess(Enum.member?(game.letters, guess))
+  end
+
+  defp score_guess(game, _scored = true) do
+    %{ game | 
+      game_state: game.letters
+        |> MapSet.new
+        |> MapSet.subset?(game.used)
+        |> maybe_won
+    }
+  end
+
+  defp score_guess(game = %{ turns_left: 1 }, _bad_guess) do
+    %{ game | game_state: :lost, turns_left: 0 }
+  end
+
+  defp score_guess(game, _bad_guess) do
+    %{ game | turns_left: game.turns_left - 1, game_state: :not_guessed }
+  end
+
+  defp maybe_won(true),   do: :won
+  defp maybe_won(_false), do: :guessed
 
   defp game_state(game) do
     %{
       turns_left: game.turns_left,
       game_state: game.game_state,
-      letters: game.letters,
+      letters: game |> reveal_guessed_letters,
       used: game.used |> MapSet.to_list() |> Enum.sort
     }
   end
-  
+
+  def return_with_state(game), do: { game, game_state(game) }
+
+  defp reveal_guessed_letters(game) do
+    game.letters 
+      |> Enum.map(fn letter -> MapSet.member?(game.used, letter) |> maybe_reveal(letter) end)
+  end
+
+  defp maybe_reveal(true, letter), do: letter
+  defp maybe_reveal(_false, _letter), do: "_"
+
 end
--- hangman/test/hangman_test.exs
+++ hangman/test/hangman_test.exs
@@ -46,4 +46,122 @@ defmodule HangmanTest do
     # then
     assert new_game == game
   end
+
+  test "letter already used" do
+    # given
+    game = Game.new_game()
+
+    { game, _state } = Game.make_guess(game, "x")
+    assert game.game_state != :used
+    
+    { game, _state } = Game.make_guess(game, "y")
+    assert game.game_state != :used
+
+    { game, _state } = Game.make_guess(game, "x")
+    assert game.game_state == :used
+    assert game.used == MapSet.new(["x", "y"])
+  end
+
+  test "guessed one letter" do
+    game = Game.new_game("wombat")
+
+    { game, _state} = Game.make_guess(game, "w")
+    assert game.game_state == :guessed
+  end
+
+  test "guessed whole word" do
+    game = Game.new_game("word")
+
+    { game, _state} = Game.make_guess(game, "w")
+    assert game.game_state == :guessed
+  end
+
+  test "not guessed" do
+    game = Game.new_game("word")
+
+    { game, _state} = Game.make_guess(game, "a")
+    assert game.game_state == :not_guessed
+  end
+
+  test "lost game" do
+    game = Game.new_game("wombat")
+
+    { game, _state} = Game.make_guess(game, "c")
+    { game, _state} = Game.make_guess(game, "d")
+    { game, _state} = Game.make_guess(game, "f")
+    { game, _state} = Game.make_guess(game, "g")
+    { game, _state} = Game.make_guess(game, "h")
+    { game, _state} = Game.make_guess(game, "i")
+    { game, _state} = Game.make_guess(game, "j")
+
+    assert game.game_state == :lost
+  end
+
+  test "won" do
+    game = Game.new_game("wombat")
+
+    { game, _state} = Game.make_guess(game, "w")
+    { game, _state} = Game.make_guess(game, "o")
+    { game, _state} = Game.make_guess(game, "m")
+    { game, _state} = Game.make_guess(game, "b")
+    { game, _state} = Game.make_guess(game, "a")
+    { game, _state} = Game.make_guess(game, "t")
+
+    assert game.game_state == :won
+  end
+
+  test "can handle moves" do
+    [
+      ["s", :guessed, 7, ["s", "_", "_", "_", "_", "_"], ["s"]],
+      ["a", :guessed, 7, ["s", "a", "_", "_", "_", "_"], ["a", "s"]],
+      ["x", :not_guessed, 6, ["s", "a", "_", "_", "_", "_"], ["a", "s", "x"]],
+      ["m", :guessed, 6, ["s", "a", "m", "_", "_", "_"], ["a", "m", "s", "x"]],
+    ]
+      |> test_moves
+  end
+
+  defp test_moves(sequence) do
+    game = Game.new_game("samuel")
+
+    Enum.reduce(sequence, game, &check_move/2)
+  end
+
+  defp check_move([ letter, state, turns_left, letters, used ], game) do
+    { game, new_state } = Game.make_guess(game, letter)
+    
+    assert new_state.game_state == state
+    assert new_state.turns_left == turns_left
+    assert new_state.letters == letters
+    assert new_state.used == used
+    
+    game
+  end
+
+  test "can handle winning game" do
+    [
+      ["s", :guessed, 7, ["s", "_", "_", "_", "_", "_"], ["s"]],
+      ["a", :guessed, 7, ["s", "a", "_", "_", "_", "_"], ["a", "s"]],
+      ["x", :not_guessed, 6, ["s", "a", "_", "_", "_", "_"], ["a", "s", "x"]],
+      ["m", :guessed, 6, ["s", "a", "m", "_", "_", "_"], ["a", "m", "s", "x"]],
+      ["u", :guessed, 6, ["s", "a", "m", "u", "_", "_"], ["a", "m", "s", "u", "x"]],
+      ["e", :guessed, 6, ["s", "a", "m", "u", "e", "_"], ["a", "e", "m", "s", "u", "x"]],
+      ["l", :won, 6, ["s", "a", "m", "u", "e", "l"], ["a", "e", "l", "m", "s", "u", "x"]],
+    ]
+      |> test_moves
+  end
+
+  test "can handle loosing game" do
+    [
+      ["s", :guessed, 7, ["s", "_", "_", "_", "_", "_"], ["s"]],
+      ["x", :not_guessed, 6, ["s", "_", "_", "_", "_", "_"], ["s", "x"]],
+      ["y", :not_guessed, 5, ["s", "_", "_", "_", "_", "_"], ["s", "x", "y"]],
+      ["z", :not_guessed, 4, ["s", "_", "_", "_", "_", "_"], ["s", "x", "y", "z"]],
+      ["w", :not_guessed, 3, ["s", "_", "_", "_", "_", "_"], ["s", "w", "x", "y", "z"]],
+      ["v", :not_guessed, 2, ["s", "_", "_", "_", "_", "_"], ["s", "v", "w", "x", "y", "z"]],
+      ["t", :not_guessed, 1, ["s", "_", "_", "_", "_", "_"], ["s", "t", "v", "w", "x", "y", "z"]],
+      ["b", :lost, 0, ["s", "_", "_", "_", "_", "_"], ["b", "s", "t", "v", "w", "x", "y", "z"]],
+    ]
+      |> test_moves
+  end
+
 end
